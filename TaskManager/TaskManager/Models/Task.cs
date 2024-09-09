31ce566e4f8fcbd9e20fe8f318bf7b7fcf405251
Kukuryzka <litonisa.uwu@gmail.com>
2024-09-09 14:49:20 +0300

    Created app

---
+++ TaskManager/TaskManager/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Category { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+
+}
---
+++ TaskManager/TaskManager/ViewModels/TaskViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Models;
+using Task = TaskManager.Models.Task;
+
+namespace TaskManager.ViewModels
+{
+    public class TaskViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Task> _tasks;
+        public ObservableCollection<Task> Tasks
+        {
+            get { return _tasks; }
+            set
+            {
+                _tasks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TaskViewModel()
+        {
+            Tasks = new ObservableCollection<Task>();
+        }
+
+        // Методы для добавления, удаления, редактирования задач и загрузки/сохранения данных 
+        // (реализуйте их в соответствии с функциональными требованиями)
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
